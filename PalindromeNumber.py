938f16ed937a7c1ce022fede14c5499ea4f3426f
saviganga <akatugbasaviour@gmail.com>
2021-07-10 07:09:18 -0700

    reverseinteger palindromenumber

---
+++ PalindromeNumber.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+''' Given an integer x, returntrue if x is a palindrome integer '''
+
+def palindromenumber(nums):
+    
+    num = nums
+    # check if number is greater than 0
+    if num < 0:
+        return False
+    
+    # reverse the number
+    rev = 0
+
+    while num > 0:
+
+        rev = (rev * 10) + (num % 10)
+        num = num // 10
+
+    if nums == rev:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    num = 100
+    palin_num = palindromenumber(num)
+    print(palin_num)
---
+++ ReversedInteger.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+''' Given a 32-bit signed integer, x, return x with its digits reversed. if reversing string causes the value to go outside the signed 32-bit-integer range [-2**31, 2**31 -1], then return 0 '''
+
+def reverseinteger(num):
+
+    len_num = len( str(num) )
+
+    # check if the number is positive
+    if num < 0:
+
+        # turn number to a string
+        s_num = str(num)
+
+        # create a list to store reversed string
+        r = []
+        
+        # reverse the string
+        while len_num-1 >= 0:
+            r.append(s_num[len_num-1])
+            len_num -= 1
+
+        # return reversed integer
+        return ''.join(r)   
+
+    # nitialize a flag number
+    rev = 0
+
+    # solve alogorithm
+    while num > 0:
+
+        rev = (rev * 10) + (num % 10)
+
+        num = num // 10
+        
+    return rev
+
+
+
+
+
+if __name__ == '__main__':
+    num = -45
+    nums = reverseinteger(num)
+    print(nums)
+    
